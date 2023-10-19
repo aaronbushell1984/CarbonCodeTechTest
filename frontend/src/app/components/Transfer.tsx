c9c378a70844e4a4028067a168417cc07f77613e
aaronbushell1984 <aaron_bushell@yahoo.com>
2023-10-19 18:20:31 +0100

    ignore ide

--- frontend/src/app/components/Transfer.tsx
+++ frontend/src/app/components/Transfer.tsx
@@ -95,7 +95,7 @@ export default function Transfer() {
                         className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline ${destinationIsEdited && !transferDestinationIsValid ? 'border-red-500' : ''}`}
                         id="destination"
                         type="text"
-                        placeholder="Ox..."
+                        placeholder="0x..."
                         disabled={loading}
                         onChange={(e) => handleTransferDestinationAddressChange(e)}
                     />
